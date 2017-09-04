6c3639e865e683a6ecd23933c677a747ecf36390
Janis Upitis <janis.upitis@knowledgeprice.com>
2017-09-04 23:41:37 +0300

    Changes in build process

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -2,4 +2,5 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	pwd
     openssl aes-256-cbc -K $encrypted_0ad6f3a71d9e_key -iv $encrypted_0ad6f3a71d9e_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    find cd/codesigning.asc
 fi
